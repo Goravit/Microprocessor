273fb3299fd4774ea7ee45d75d473a662e041408
Goravit1116 <6201111@kmitl.ac.th>
2020-02-06 14:51:03 +0700

    Create Micro_Lab_3_Ex_2.ino

---
+++ MicroLab Week 3/Micro_Lab_3_Ex_2/Micro_Lab_3_Ex_2.ino
@@ -0,0 +1,57 @@
+int clk_pin=13;//SHCP
+int latch_pin=10;//STCP
+int data_pin=11;//DS
+
+int but1 = 9;
+int but2 = 8;
+
+int i = 0;
+
+int num;
+
+void setLed(uint8_t _state){
+  digitalWrite(latch_pin,LOW);
+  for(int i=0;i<8;i++){
+    digitalWrite(data_pin,(_state>>(7-i))&0x01);
+    digitalWrite(clk_pin,HIGH);
+    digitalWrite(clk_pin,LOW);
+  }
+  digitalWrite(latch_pin,HIGH);
+}
+
+
+int map7seg[10] = {
+  0b00111111,
+  0b00000110,
+  0b01011011,
+  0b01001111,
+  0b01100110,
+  0b01101101,
+  0b01111101,
+  0b00000111,
+  0b01111111,
+  0b01100111,
+};
+
+
+void setup()
+{
+  //putyoursetupcodehere,torunonce:
+  pinMode(clk_pin,OUTPUT);
+  pinMode(latch_pin,OUTPUT);
+  pinMode(data_pin,OUTPUT);
+  pinMode(but1,INPUT_PULLUP);
+  pinMode(but2,INPUT_PULLUP);
+  Serial.begin(9600);
+  setLed(0x3F);
+}
+
+void loop() 
+{ 
+   if (Serial.available() > 0) 
+   {
+    char byt = Serial.read();
+     i = byt-'0';
+     setLed(map7seg[i]);
+   }   
+}
